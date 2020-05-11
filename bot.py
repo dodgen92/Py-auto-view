4a1fa45a34b6219323062cfd33c634579b1ee35a
Tyler Dodgen <tylerremingtondodgen@gmail.com>
2020-05-11 02:29:06 -0400

    commented code

--- bot.py
+++ bot.py
@@ -2,14 +2,17 @@ import webbrowser
 import time
 from pykeyboard import PyKeyboard
 
+#replace "#" with url website
 count = 0
 urls = ['#']
 k = PyKeyboard()
 
+#replace count< with how many times to visit
 while count < 100:
     for url in urls:
         webbrowser.open(url, new=0)
-        time.sleep(10)
+# replace "time.sleep" with frequency of repeat views 
+         time.sleep(10)
         k.press_keys(['Command','W'])
         count = count + 1
 
